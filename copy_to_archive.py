903002425aa32963398117800c5322ed22953de3
Sasha <Sasha7b9@tut.by>
2015-10-17 00:35:12 +0300

    test

---
+++ copy_to_archive.py
@@ -0,0 +1,23 @@
+import os
+import datetime
+
+def RunCommands(commands):
+    for command in commands:
+        print(command)
+        os.system(command)
+
+# Start Here
+strings =   "del Debug\\*.* /q", \
+            "del Debug\\TacticsVictory.tlog\\*.* /s /q"
+
+RunCommands(strings)
+
+date = datetime.datetime.now()
+
+name = ':\\Archiv\\TacticsVictory\\TacticsVictory' + date.strftime("_%Y_%m_%d_%H_%M_%S") + '.zip .'
+
+strings =   "7z.exe a " + 'c' + name, \
+            "7z.exe a " + 'f' + name, \
+            "pause"
+
+RunCommands(strings)
