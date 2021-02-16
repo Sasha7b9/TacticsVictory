23f63cc087c9b569233f4367d93c3b35487acba2
Sasha7b9 <Sasha7b9@tut.by>
2021-02-16 20:49:58 +0300

    refact

--- src/common_files/GUI/Controls/_GovernorFloat.h
+++ src/common_files/GUI/Controls/_GovernorFloat.h
@@ -1,6 +1,6 @@
 ﻿/* (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by */
 #pragma once
-#include "GUI/Controls/_WindowRTS.h"
+#include "GUI/Controls/_WindowTV.h"
 
 
 enum CellType
--- src/local/BattlerTV/GUI/Panels/PanelBottom.h
+++ src/local/BattlerTV/GUI/Panels/PanelBottom.h
@@ -1,6 +1,6 @@
 // (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
-#include "GUI/Controls/_WindowRTS.h"
+#include "GUI/Controls/_WindowTV.h"
 
 
 /*
