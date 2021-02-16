70f6e5d797a007d2b256c0b947592d69d6542b55
Sasha7b9 <Sasha7b9@tut.by>
2021-02-16 17:04:25 +0300

    refact

--- src/common_files/Scene/Objects/Ammo/_AmmoObject.h
+++ src/common_files/Scene/Objects/Ammo/_AmmoObject.h
@@ -8,3 +8,11 @@ class AmmoObject : public GameObject
 public:
     AmmoObject(Context *context = TheContext);
 };
+
+
+#include "Scene/Objects/Ammo/_AirBomb.h"
+#include "Scene/Objects/Ammo/_Grenade.h"
+#include "Scene/Objects/Ammo/_Mine.h"
+#include "Scene/Objects/Ammo/_Rocket.h"
+#include "Scene/Objects/Ammo/_RollingBomb.h"
+
