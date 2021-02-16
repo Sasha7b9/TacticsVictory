716f7166cc797dc52000ad5e82a2e22ee359f09e
Sasha7b9 <Sasha7b9@tut.by>
2021-02-16 17:17:50 +0300

    *

--- src/common_files/Scene/Objects/Buildings/_BuildingObject.h
+++ src/common_files/Scene/Objects/Buildings/_BuildingObject.h
@@ -3,9 +3,16 @@
 #include "Scene/Objects/_GameObject.h"
 
 
-
 class BuildingObject : public GameObject
 {
 public:
     BuildingObject(Context *context = TheContext);
 };
+
+
+#include "Scene/Objects/Buildings/_CivilPlant.h"
+#include "Scene/Objects/Buildings/_MilitaryPlant.h"
+#include "Scene/Objects/Buildings/_PowerPlant.h"
+#include "Scene/Objects/Buildings/_ProcessinPlant.h"
+#include "Scene/Objects/Buildings/_Shaft.h"
+#include "Scene/Objects/Buildings/_WarehouseAmmo.h"
