de65e9452962a575d3d183299556c7b99f55f6a8
Sasha7b9 <Sasha7b9@tut.by>
2021-02-18 12:12:26 +0300

    *

--- src/local/TacticsVictory/TTacticsVictoryHandlers.cpp
+++ src/local/TacticsVictory/TTacticsVictoryHandlers.cpp
@@ -1,5 +1,6 @@
 ﻿// (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "stdafx.h"
+#include "TTacticsVictory.h"
 
 
 void TacticsVictory::HandlePostRenderUpdate(StringHash, VariantMap&)
--- src/local/TacticsVictory/stdafx.h
+++ src/local/TacticsVictory/stdafx.h
@@ -125,9 +125,6 @@ using namespace Urho3D;
 #include "glob_engine.h"
 #include "glob_game.h"
 
-
-#include "TTacticsVictory.h"
-
 #include "Audio/TSounds.h"
 
 #include "Core/_TMath.h"
