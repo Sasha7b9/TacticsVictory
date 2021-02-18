9e388337e264f3b53ec8352da70ac843df55ac57
Sasha7b9 <Sasha7b9@tut.by>
2021-02-18 09:30:26 +0300

    refact

--- src/local/BattlerTV/glob_game.h
+++ src/local/BattlerTV/glob_game.h
@@ -1,55 +1,46 @@
 // 2021/02/15 16:12:30 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by 
 #pragma once
 
+class TCamera;
 class TConsole;
 class TCursor;
-class Editor;
-class GUI;
-class GuiEditor;
-class GuiGame;
-class Hint;
-class Level;
-class MenuGame;
-class MenuStart;
-class MenuOptions;
-class SegmentTerrain;
-class Settings;
-class Tank;
+class  Editor;
+class  GUI;
+class  GuiEditor;
+class  GuiGame;
+class  Hint;
+class  Level;
+class CLog;
+class  MenuConfirmExit;
+class  MenuGame;
+class TMenu;
+class TScene;
+class  Settings;
+class  Battler;
 class TTerrain;
 class TWindow;
-class MenuConfirmExit;
-class WindowTarget;
-class WindowVariables;
-class Battler;
-class TMenu;
-class Server;
-class Client;
+class  WindowTarget;
+class  WindowVariables;
 
-class TCamera;
-class CLog;
-class TScene;
-
-extern TCamera         *TheCamera;
-extern Client          *TheClient;
-extern TConsole       *TheConsole;
-extern TCursor        *TheCursor;
-extern Editor          *TheEditor;
-extern GUI             *TheGUI;
-extern GuiEditor       *TheGuiEditor;
-extern GuiGame         *TheGuiGame;
-extern Hint            *TheHint;
-extern Level           *TheLevel;
-extern CLog            *TheLog;
-extern MenuConfirmExit *TheMenuConfirmExit;
-extern MenuGame        *TheMenuMain;
-extern TMenu          *TheMenu;
-extern TScene          *TheScene;
-extern Server          *TheServer;
-extern Settings        *TheSet;
-extern Battler         *TheTacticsVictory;
-extern TTerrain       *TheTerrain;
-extern WindowTarget    *TheWindowTarget;             // В этом окне можно посмотреть, что видит юнит
-extern WindowVariables *TheWindowVars;
+extern TCamera          *TheCamera;
+extern TConsole         *TheConsole;
+extern TCursor          *TheCursor;
+extern  Editor          *TheEditor;
+extern  GUI             *TheGUI;
+extern  GuiEditor       *TheGuiEditor;
+extern  GuiGame         *TheGuiGame;
+extern  Hint            *TheHint;
+extern  Level           *TheLevel;
+extern CLog             *TheLog;
+extern  MenuConfirmExit *TheMenuConfirmExit;
+extern  MenuGame        *TheMenuMain;
+extern TMenu            *TheMenu;
+extern TScene           *TheScene;
+extern  Settings        *TheSet;
+extern  Battler         *TheTacticsVictory;
+extern TTerrain         *TheTerrain;
+extern  WindowTarget    *TheWindowTarget;             // В этом окне можно посмотреть, что видит юнит
+extern  WindowVariables *TheWindowVars;
 
 extern int *TheCounterHint;
 extern Vector<TWindow *> TheOpenedWindow;
