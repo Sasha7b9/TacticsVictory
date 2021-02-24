a54aa0b107ab9084513905f518af4440ea1b5e0d
Sasha7b9 <Sasha7b9@tut.by>
2021-02-24 13:41:04 +0300

    refact

--- src/common_files/Game/Path/_TWaveAlgorithm.cpp
+++ src/common_files/Game/Path/_TWaveAlgorithm.cpp
@@ -1,6 +1,6 @@
 ﻿/* (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by */
 #include "stdafx.h"
-#include "Game/Path/TWaveAlgorithm.h"
+#include "Game/Path/_TWaveAlgorithm.h"
 
 
 #define TERRAIN_HEIGHT_EQUAL(x,y) (fabs(TheTerrain->GetHeight(x, y) - heightStart) < M_EPSILON)
