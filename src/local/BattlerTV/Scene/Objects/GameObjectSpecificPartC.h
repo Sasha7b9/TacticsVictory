55c40021e45bab7abfe90d7835084c3dc49122d3
Sasha7b9 <Sasha7b9@tut.by>
2021-02-25 21:18:10 +0300

    *

---
+++ src/local/BattlerTV/Scene/Objects/GameObjectSpecificPartC.h
@@ -0,0 +1,10 @@
+// 2021/02/25 21:15:57 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+#include "Scene/Objects/GameObjectSpecificPart_.h"
+
+
+class GameObjectSpecificPartC : public GameObjectSpecificPart
+{
+    GameObjectSpecificPartC() : GameObjectSpecificPart() {}
+};
+
