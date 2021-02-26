afee98f64891e737920aefbd1b6e80eeecfdb989
Sasha7b9 <Sasha7b9@tut.by>
2021-02-26 15:57:02 +0300

    *

---
+++ src/local/BattlerTV/Scene/Objects/Units/UnitObjectC.cpp
@@ -0,0 +1,4 @@
+// 2021/02/26 15:54:30 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "stdafx.h"
+#include "Scene/Objects/Units/UnitObjectC.h"
+
---
+++ src/local/BattlerTV/Scene/Objects/Units/UnitObjectC.h
@@ -0,0 +1,13 @@
+// 2021/02/26 15:53:20 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+#include "Scene/Objects/Units/UnitObject_.h"
+
+
+class UnitObjectSpecificPartC : public UnitObjectSpecificPart
+{
+    URHO3D_OBJECT(UnitObjectSpecificPartC, UnitObjectSpecificPart);
+
+public:
+
+protected:
+};
