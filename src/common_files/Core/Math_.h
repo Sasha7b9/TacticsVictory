58af596ecf74d20c26167db6e5bcec67d2c05735
Sasha7b9 <Sasha7b9@tut.by>
2021-02-27 22:02:48 +0300

    *

--- src/common_files/Core/Math_.h
+++ src/common_files/Core/Math_.h
@@ -11,7 +11,12 @@ public:
     static int CircularIncrease(int value, int min, int max);
     template <class T> static int Sign(T val)
     {
-        if(val == T(0)) return 0; return val < 0 ? -1 : 1;
+        if (val == T(0))
+        {
+            return 0;
+        }
+        
+        return val < 0 ? -1 : 1;
     }
 
     template <class T> static bool PointsAreLocatedOneAfterAnother(T one, T two, T three)
