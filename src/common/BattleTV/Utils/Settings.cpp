008e5bc4eb2aabb58dc560590890cb03f90aa876
Sasha7b9 <Sasha7b9@tut.by>
2021-02-27 08:59:35 +0300

    *

--- src/common/BattleTV/Utils/Settings.cpp
+++ src/common/BattleTV/Utils/Settings.cpp
@@ -1,7 +1,7 @@
 // (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "stdafx.h"
 #include "Utils/GlobalFunctions.h"
-#include "Utils/TSettings.h"
+#include "Utils/Settings.h"
 
 
 bool operator==(const Settings::IntKey & keyleft, const Settings::IntKey& keyright)
