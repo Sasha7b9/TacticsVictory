2a6ea7a7f1915718edc51330ca254228f9dda49c
Sasha7b9 <Sasha7b9@tut.by>
2021-02-27 22:13:36 +0300

    *

--- src/common/BattleTV/Utils/Settings.h
+++ src/common/BattleTV/Utils/Settings.h
@@ -30,7 +30,7 @@ public:
     {
         IntKey()
         {};
-        IntKey(char *child, pchar elem = "val")
+        IntKey(pchar child, pchar elem = "val")
         {
             str = String(child) + String(elem);
         };
