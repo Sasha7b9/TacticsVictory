45007472f9ad6cd3e8c93996e1bcc4c87a06d35f
Sasha7b9 <Sasha7b9@tut.by>
2021-02-27 17:48:37 +0300

    refact. Battler собирается

--- src/common_files/Scene/Terrain/LayerTerrain_.cpp
+++ src/common_files/Scene/Terrain/LayerTerrain_.cpp
@@ -1,10 +1,7 @@
 // (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "stdafx.h"
 #include "Core/Math_.h"
-#include "Scene/Scene_.h"
-#ifdef CLIENT
 #include "Scene/SceneC.h"
-#endif
 #include "Scene/Terrain/LayerTerrain_.h"
 
 
