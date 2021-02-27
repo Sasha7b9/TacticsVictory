5d46f5ca5efe366a385b96c917ec3c3eff516d91
Sasha7b9 <Sasha7b9@tut.by>
2021-02-27 23:53:01 +0300

    *

--- src/local/BattlerTV/glob_game.h
+++ src/local/BattlerTV/glob_game.h
@@ -24,26 +24,26 @@ class WindowTarget;
 class WindowVariables;
 class WindowT;
 
-extern CameraT *TheCamera;
-extern ConsoleT *TheConsole;
-extern CursorT *TheCursor;
-extern Editor *TheEditor;
-extern GUI *TheGUI;
-extern GuiEditor *TheGuiEditor;
-extern GuiGame *TheGuiGame;
-extern Hint *TheHint;
-extern Level *TheLevel;
-extern LogC *TheLog;
-extern MenuConfirmExit *TheMenuConfirmExit;
-extern MenuGame *TheMenuMain;
-extern MenuT *TheMenu;
-extern Mouse *TheMouse;
-extern PathIndicator *ThePathIndicator;
-extern SceneC *TheScene;
-extern Settings *TheSet;
-extern Battler *TheBattler;
-extern TerrainT *TheTerrain;
-extern WindowTarget *TheWindowTarget;             // В этом окне можно посмотреть, что видит юнит
-extern WindowVariables *TheWindowVars;
+extern CameraT          *TheCamera;
+extern ConsoleT         *TheConsole;
+extern CursorT          *TheCursor;
+extern Editor           *TheEditor;
+extern GUI              *TheGUI;
+extern GuiEditor        *TheGuiEditor;
+extern GuiGame          *TheGuiGame;
+extern Hint             *TheHint;
+extern Level            *TheLevel;
+extern LogC             *TheLog;
+extern MenuConfirmExit  *TheMenuConfirmExit;
+extern MenuGame         *TheMenuMain;
+extern MenuT            *TheMenu;
+extern Mouse            *TheMouse;
+extern PathIndicator    *ThePathIndicator;
+extern SceneC           *TheScene;
+extern Settings         *TheSet;
+extern Battler          *TheBattler;
+extern TerrainT         *TheTerrain;
+extern WindowTarget     *TheWindowTarget;             // В этом окне можно посмотреть, что видит юнит
+extern WindowVariables  *TheWindowVars;
 
 extern Vector<WindowT *> TheOpenedWindow;
