339321e62056e0e12b9e2015e5ddde115a851918
Sasha7b9 <Sasha7b9@tut.by>
2021-02-27 23:45:14 +0300

    *

--- src/common/BattleTV/glob_game.h
+++ src/common/BattleTV/glob_game.h
@@ -1,12 +1,12 @@
 // 2021/02/15 16:12:30 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by 
 #pragma once
 
-class  Battle;
-class  Level;
-class  LogT;
-class  SceneT;
-class  Settings;
-class  TerrainT;
+class Battle;
+class Level;
+class LogT;
+class SceneT;
+class Settings;
+class TerrainT;
 
 extern Battle     *TheBattle;
 extern Level      *TheLevel;
--- src/local/BattlerTV/glob_game.h
+++ src/local/BattlerTV/glob_game.h
@@ -1,49 +1,49 @@
 // 2021/02/15 16:12:30 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by 
 #pragma once
 
-class  CameraT;
-class  ConsoleT;
-class  CursorT;
-class  Editor;
-class  GUI;
-class  GuiEditor;
-class  GuiGame;
-class  Hint;
-class  Level;
-class  LogC;
-class  MenuConfirmExit;
-class  MenuGame;
-class  MenuT;
-class  Mouse;
-class  PathIndicator;
-class  SceneC;
-class  Settings;
-class  Battler;
-class  TerrainT;
-class  WindowTarget;
-class  WindowVariables;
+class CameraT;
+class ConsoleT;
+class CursorT;
+class Editor;
+class GUI;
+class GuiEditor;
+class GuiGame;
+class Hint;
+class Level;
+class LogC;
+class MenuConfirmExit;
+class MenuGame;
+class MenuT;
+class Mouse;
+class PathIndicator;
+class SceneC;
+class Settings;
+class Battler;
+class TerrainT;
+class WindowTarget;
+class WindowVariables;
 class WindowT;
 
 extern CameraT *TheCamera;
 extern ConsoleT *TheConsole;
 extern CursorT *TheCursor;
-extern  Editor *TheEditor;
-extern  GUI *TheGUI;
-extern  GuiEditor *TheGuiEditor;
-extern  GuiGame *TheGuiGame;
-extern  Hint *TheHint;
-extern  Level *TheLevel;
-extern  LogC *TheLog;
-extern  MenuConfirmExit *TheMenuConfirmExit;
-extern  MenuGame *TheMenuMain;
+extern Editor *TheEditor;
+extern GUI *TheGUI;
+extern GuiEditor *TheGuiEditor;
+extern GuiGame *TheGuiGame;
+extern Hint *TheHint;
+extern Level *TheLevel;
+extern LogC *TheLog;
+extern MenuConfirmExit *TheMenuConfirmExit;
+extern MenuGame *TheMenuMain;
 extern MenuT *TheMenu;
-extern  Mouse *TheMouse;
-extern  PathIndicator *ThePathIndicator;
+extern Mouse *TheMouse;
+extern PathIndicator *ThePathIndicator;
 extern SceneC *TheScene;
-extern  Settings *TheSet;
-extern  Battler *TheBattler;
+extern Settings *TheSet;
+extern Battler *TheBattler;
 extern TerrainT *TheTerrain;
-extern  WindowTarget *TheWindowTarget;             // В этом окне можно посмотреть, что видит юнит
-extern  WindowVariables *TheWindowVars;
+extern WindowTarget *TheWindowTarget;             // В этом окне можно посмотреть, что видит юнит
+extern WindowVariables *TheWindowVars;
 
 extern Vector<WindowT *> TheOpenedWindow;
