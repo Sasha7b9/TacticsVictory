cb48cd3d44710ccff1f22c7acae2d894fd95ed51
Sasha7b9 <Sasha7b9@tut.by>
2021-03-01 13:22:09 +0300

    Добавлен файл Accessories

---
+++ src/common_files/Scene/Objects/Units/Accessories/Accessories_.cpp
@@ -0,0 +1,10 @@
+// 2021/03/01 12:59:18 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "stdafx.h"
+#include "Scene/Objects/Units/Accessories/Accessories_.h"
+#include "Scene/Objects/Units/Accessories/Engine/Engine_.h"
+
+
+void Accessories::Register()
+{
+    TheContext->RegisterFactory<EngineT>();
+}
---
+++ src/common_files/Scene/Objects/Units/Accessories/Accessories_.h
@@ -0,0 +1,8 @@
+// 2021/03/01 12:59:01 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+class Accessories
+{
+    static void Register();
+};
