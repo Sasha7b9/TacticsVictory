595aab48617520be935c2b5cf24865f87584c595
Sasha7b9 <Sasha7b9@tut.by>
2021-03-02 14:52:37 +0300

    *

--- src/common/BattleTV/Battle.cpp
+++ src/common/BattleTV/Battle.cpp
@@ -94,7 +94,7 @@ void Battle::Start()
 
     TheScene->Create();
 
-    for (int i = 0; i < 5000; i++)
+    for (int i = 0; i < 2500; i++)
     {
         uint colZ = (uint)Random((float)TheTerrain->WidthZ());
         uint rowX = (uint)Random((float)TheTerrain->HeightX());
