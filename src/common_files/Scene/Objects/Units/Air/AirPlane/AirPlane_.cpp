eae6e0e16e0fad33a2137d20a8f4a4ee068ba3c7
Sasha7b9 <Sasha7b9@tut.by>
2021-03-03 19:29:43 +0300

    *

--- src/common_files/Scene/Objects/Units/Air/AirPlane/AirPlane_.cpp
+++ src/common_files/Scene/Objects/Units/Air/AirPlane/AirPlane_.cpp
@@ -1,3 +1,3 @@
 // 2021/02/22 20:46:32 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "stdafx.h"
-#include "Scene/Objects/Units/AirPlane/AirPlane_.h"
+#include "Scene/Objects/Units/Air/AirPlane/AirPlane_.h"
