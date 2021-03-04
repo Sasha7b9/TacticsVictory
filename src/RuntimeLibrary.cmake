bbf672eced33069fbc1ab72e339a7b34d133e9d8
Sasha7b9 <Sasha7b9@tut.by>
2021-03-04 10:41:52 +0300

    *

--- src/RuntimeLibrary.cmake
+++ src/RuntimeLibrary.cmake
@@ -1,11 +1,9 @@
 
 if(WIN32)
     if(${CMAKE_BUILD_TYPE} STREQUAL Release)        # Для релизной версии
-        message("............................... Release mode")
         set_property(TARGET ${PROJECT_NAME} PROPERTY
             MSVC_RUNTIME_LIBRARY "MultiThreaded")   # Устанавливаем статическую линковку рантайм-библиотек
     elseif(${CMAKE_BUILD_TYPE} STREQUAL Debug)
-        message("............................... Debug mode")
         set_property(TARGET ${PROJECT_NAME} PROPERTY
             MSVC_RUNTIME_LIBRARY "MultiThreadedDebugDLL")
     endif()
