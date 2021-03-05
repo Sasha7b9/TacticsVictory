f24ccbac756fa5084421b7a633d5f75571e4ffbf
Sasha7b9 <Sasha7b9@tut.by>
2021-03-05 19:49:47 +0300

    *

--- src/common_files/Scene/Objects/Object_.cpp
+++ src/common_files/Scene/Objects/Object_.cpp
@@ -103,13 +103,13 @@ void ObjectT::Normalize(float k)
 
 Vector3 PhysicsParameters::Position::Get() const
 {
-    return object->GetNode()->GetPosition() - object->shift->position;
+    return object->GetNode()->GetPosition() /*- object->shift->position*/;
 }
 
 
 void PhysicsParameters::Position::Set(const Vector3 &position)
 {
-    object->GetNode()->SetPosition(position + object->shift->position);
+    object->GetNode()->SetPosition(position /* + object->shift->position*/);
 }
 
 
