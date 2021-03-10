eaab64b8868a85e328f07ce93bad9772997624c6
Sasha7b9 <Sasha7b9@tut.by>
2021-03-10 17:07:17 +0300

    *

--- out/debug/TVData/convert_model.bat
+++ out/debug/TVData/convert_model.bat
@@ -1,3 +1,3 @@
 PATH = %PATH%;d:\My\Urho3D\bin\;d:\My\Urho3D\generated\debug\bin\tool\;
-AssetImporter model Models\Units\Plane\Plane.obj Models\Units\Plane\Plane   .mdl -l -nf -t -ct
+AssetImporter model Models\Units\Air\AirPlane\AirPlane.obj Models\Units\Air\AirPlane\AirPlane.mdl -l -nf -t -ct
 pause
