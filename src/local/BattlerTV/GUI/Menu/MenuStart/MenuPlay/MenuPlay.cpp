4244d6ba13ade31acf69b8d1e65c05ab153c4611
Sasha7b9 <Sasha7b9@tut.by>
2021-03-16 21:54:38 +0300

    *

---
+++ src/local/BattlerTV/GUI/Menu/MenuStart/MenuPlay/MenuPlay.cpp
@@ -0,0 +1,3 @@
+// 2021/03/16 21:49:45 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "stdafx.h"
+#include "GUI/Menu/MenuStart/MenuPlay/MenuPlay.h"
---
+++ src/local/BattlerTV/GUI/Menu/MenuStart/MenuPlay/MenuPlay.h
@@ -0,0 +1,12 @@
+// 2021/03/16 21:50:02 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+#include "GUI/Menu/WindowMenu.h"
+
+
+class MenuPlay : public WindowMenu
+{
+    URHO3D_OBJECT(MenuPlay, WindowMenu);
+
+public:
+    MenuPlay(Context *);
+};
