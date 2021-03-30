49fd1d18535b5dd17635db0031eda3dee044c006
Sasha7b9 <Sasha7b9@tut.by>
2021-03-30 21:27:44 +0300

    Собирается Battle

--- src/common_files/Utils/Log_.h
+++ src/common_files/Utils/Log_.h
@@ -1,41 +1,13 @@
-﻿// (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+// 2021/03/30 21:23:50 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
 
 
-class LogT : public Log
-{
-    URHO3D_OBJECT(LogT, Log);
+#ifdef LOG_U3D
 
-public:
-    LogT(LogT **self);
-    virtual ~LogT();
-    virtual void Write(int level, const String &message, pchar file, pchar func, int numLine);
-    void EnableExtendedInfo();
-    void DisableExtendedInfo();
+#include "Utils/LogU3D_.h"
 
-protected:
-    static bool enabledExtendedInfo;
-};
+#else
 
 
-struct ConsoleLog
-{
-    static void Create();
 
-    static void Write(pchar message);
-
-    static void Destroy();
-
-#ifdef WIN32
-    static HANDLE handle;
 #endif
-};
-
-
-#define LOGINFO(message)        TheLog->Write(LOG_INFO, message, __FILE__, __FUNCTION__, __LINE__)
-#define LOGINFOF(format, ...)   TheLog->Write(LOG_INFO, ToString(format, ##__VA_ARGS__), __FILE__, __FUNCTION__, __LINE__)
-#define LOGERROR(message)       TheLog->Write(LOG_ERROR, message, __FILE__, __FUNCTION__, __LINE__)
-#define LOGERRORF(format, ...)  TheLog->Write(LOG_ERROR, ToString(format, ##__VA_ARGS__), __FILE__, __FUNCTION__, __LINE__)
-
-//#define LOG_FUNC_ENTER()  URHO3D_LOGINFOF(">>>>>>>>>>>>>>>            %s", __FUNCTION__);
-#define LOG_FUNC_ENTER() LOGINFOF(">>>>>>>>>>>>>>>            %s", __FUNCTION__)
