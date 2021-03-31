5779aa23f13af876268a6d4433136fee26f9083c
Sasha7b9 <Sasha7b9@tut.by>
2021-03-31 17:06:03 +0300

    Наконец-то всё линкуется

--- src/RuntimeLibrary.cmake
+++ src/RuntimeLibrary.cmake
@@ -1,10 +1,10 @@
 
 if(WIN32)
     if(${CMAKE_BUILD_TYPE} STREQUAL Release)        # Для релизной версии
-        message("...................................................MultiThreaded")
-        set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded")
+        set_property(TARGET ${PROJECT_NAME} PROPERTY
+            MSVC_RUNTIME_LIBRARY "MultiThreaded")   # Устанавливаем статическую линковку рантайм-библиотек
     elseif(${CMAKE_BUILD_TYPE} STREQUAL Debug)
-        message("...................................................MultiThreadedDebugDLL")
-        set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreadedDebugDLL")
+        set_property(TARGET ${PROJECT_NAME} PROPERTY
+            MSVC_RUNTIME_LIBRARY "MultiThreadedDebugDLL")
     endif()
 endif()
