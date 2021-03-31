56192fd3f112f76fe9888dec48a3fbcfd2a0aee6
Sasha7b9 <Sasha7b9@tut.by>
2021-03-31 14:55:40 +0300

    Никак не получается сделать static в Release-версии

--- src/RuntimeLibrary.cmake
+++ src/RuntimeLibrary.cmake
@@ -1,9 +1,11 @@
 
 if(WIN32)
     if(${CMAKE_BUILD_TYPE} STREQUAL Release)        # Для релизной версии
+        message("...................................................MultiThreaded")
         set_property(TARGET ${PROJECT_NAME} PROPERTY
             MSVC_RUNTIME_LIBRARY "MultiThreaded")   # Устанавливаем статическую линковку рантайм-библиотек
     elseif(${CMAKE_BUILD_TYPE} STREQUAL Debug)
+        message("...................................................MultiThreadedDebugDLL")
         set_property(TARGET ${PROJECT_NAME} PROPERTY
             MSVC_RUNTIME_LIBRARY "MultiThreadedDebugDLL")
     endif()
