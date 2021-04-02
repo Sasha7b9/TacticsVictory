900970bb8252033bf486772cb5032fed127d7483
Sasha7b9 <Sasha7b9@tut.by>
2021-04-02 17:43:39 +0300

    Начало Client.exe

---
+++ src/local/Client/Battler.cpp
@@ -0,0 +1,250 @@
+﻿// (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "stdafx.h"
+#include "Battler.h"
+#include "Editor/Editor.h"
+#include "Game/Logic/Rotator_.h"
+#include "Game/Logic/SunEngine_.h"
+#include "Game/Path/TilePath.h"
+#include "Graphics/2D/Image.h"
+#include "Graphics/3D/TileSelected.h"
+#include "GUI/Cursor.h"
+#include "GUI/GUI.h"
+#include "GUI/GuiEditor/GuiEditor.h"
+#include "GUI/Menu/MenuEvents.h"
+#include "GUI/Menu/Menu.h"
+#include "Input/Mouse.h"
+#include "Network/ServerC.h"
+#include "Scene/SceneC.h"
+#include "Scene/Cameras/Camera.h"
+#include "Scene/Objects/Object_.h"
+#include "Scene/Objects/Units/Air/AirPlane/AirPlane_.h"
+#include "Scene/Objects/Units/Ground/Tank/Tank_.h"
+#include "Utils/Log_.h"
+#include "Utils/Settings.h"
+#include "Utils/SettingsTypes.h"
+
+
+#pragma warning(push)
+#pragma warning(disable:4100)
+URHO3D_DEFINE_APPLICATION_MAIN(Battler)
+#pragma warning(pop)
+
+
+Battler::Battler(Context* context) :
+    Application(context)
+{
+    TheContext = context;
+}
+
+
+void Battler::Setup()
+{
+    TheBattler = this;
+
+    GetSubsystems();
+
+    settings = new Settings(&TheSet);
+
+    TheSet->Load();
+
+    TuneEngineParameters();
+}
+
+
+void Battler::GetSubsystems()
+{
+    TheCache = GetSubsystem<ResourceCache>();
+    TheFileSystem = GetSubsystem<FileSystem>();
+    TheTime = GetSubsystem<Time>();
+    TheProfiler = GetSubsystem<Profiler>();
+    TheEngine = GetSubsystem<Engine>();
+    TheGraphics = GetSubsystem<Graphics>();
+    TheRenderer = GetSubsystem<Renderer>();
+    TheAudio = GetSubsystem<Audio>();
+    TheUI = GetSubsystem<UI>();
+    TheInput = GetSubsystem<Input>();
+    TheLocalization = GetSubsystem<Localization>();
+    TheNetwork = GetSubsystem<Network>();
+
+    CreateScriptSystem();
+}
+
+
+void Battler::TuneEngineParameters()
+{
+    engineParameters_[EP_WINDOW_TITLE] = GetTypeName();
+    engineParameters_[EP_LOG_NAME] = GetSubsystem<FileSystem>()->
+                                                        GetAppPreferencesDir("urho3d", "logs") + GetTypeName() + ".log";
+    engineParameters_[EP_FULL_SCREEN] = false;
+    engineParameters_[EP_TEXTURE_QUALITY] = 32; //-V112
+    engineParameters_[EP_WINDOW_WIDTH] = TheSet->GetInt(TV_SCREEN_WIDTH);
+    engineParameters_[EP_WINDOW_HEIGHT] = TheSet->GetInt(TV_SCREEN_HEIGHT);
+    engineParameters_[EP_HEADLESS] = false;
+
+    if (!engineParameters_.Contains(EP_RESOURCE_PREFIX_PATHS))
+#ifdef DEBUG
+        engineParameters_[EP_RESOURCE_PREFIX_PATHS] = ";../../../../../../out/debug";
+#else
+        engineParameters_[EP_RESOURCE_PREFIX_PATHS] = ";../../../../../../out/release";
+#endif
+
+    TheCache->AddResourceDir(RESOURCES_DIR);
+}
+
+
+void Battler::Start()
+{
+    OpenLog();
+
+    PROFILER_FUNC_ENTER();
+
+    GetSubsystems();
+
+    Application::Start();
+
+    SetLocalization();
+    TheFont = TheCache->GetResource<Font>(SET::MENU::FONT::NAME);
+
+    RegistrationObjects();
+
+    mouse = new Mouse(&TheMouse);
+
+    SetWindowTitleAndIcon();
+
+    CreateConsoleAndDebugHud();
+
+    SubscribeToEvents();
+
+    server = new ServerC(&TheServer);
+
+    TheServer->Connect(SERVER_ADDRESS, SERVER_PORT);
+
+    scene = new SceneC(&TheScene);
+
+    TheScene->CreateComponent<DebugRenderer>(LOCAL);
+    TheDebugRenderer = TheScene->GetComponent<DebugRenderer>();
+
+    CreateGUI();
+
+    LOGINFO("Загружаю настройки");
+    menu = new MenuT(&TheMenu);
+    TheFileSelector = new FileSelector(TheContext);
+    TheFileSelector->GetWindow()->SetModal(false);
+    TheFileSelector->GetWindow()->SetVisible(false);
+
+    PROFILER_FUNC_LEAVE();
+}
+
+
+void Battler::CreateGUI()
+{
+    TheUIRoot = TheUI->GetRoot();
+    TheUIRoot->SetDefaultStyle(TheCache->GetResource<XMLFile>("UI/MainStyle.xml"));
+    gui = new GUI(&TheGUI);
+}
+
+
+void Battler::Stop()
+{
+    engine_->DumpResources(true);
+    engine_->DumpProfiler();
+
+    TilePath::RemoveAll();
+
+    delete ThePathIndicator;
+    delete TheFileSelector;
+}
+
+
+void Battler::SetLocalization()
+{
+    TheLocalization->LoadJSONFile("Strings.json");
+    TheLocalization->SetLanguage("ru");
+}
+
+
+static void MessageCallback(const asSMessageInfo *msg, void *)
+{
+    const char *type = "AS ERROR ";
+    if (msg->type == asMSGTYPE_WARNING)
+    {
+        type = "AS WARN ";
+    }
+    else if (msg->type == asMSGTYPE_INFORMATION)
+    {
+        type = "AS INFO ";
+    }
+
+    LOGINFOF("%s (%d, %d) : %s : %s\n", msg->section, msg->row, msg->col, type, msg->message); //-V111
+}
+
+
+void Battler::CreateScriptSystem()
+{
+    TheContext->RegisterSubsystem(new Script(TheContext));
+    TheScript = GetSubsystem<Script>();
+    TheScript->GetScriptEngine()->SetMessageCallback(asFUNCTION(MessageCallback), 0, asCALL_CDECL);
+}
+
+
+void Battler::RegistrationObjects()
+{
+    CameraT::RegisterObject();
+    CursorT::RegisterObject();
+    ImageT::RegisterObject();
+    SunEngine::RegisterObject();
+    Rotator::RegisterObject();
+    WaveAlgorithm::RegisterObject();
+    TileSelected::RegisterObject();
+    AirPlane::RegisterObject();
+    Tank::RegisterObject();
+}
+
+
+void Battler::SubscribeToEvents()
+{
+    SubscribeToEvent(E_KEYDOWN, URHO3D_HANDLER(Battler, HandleKeyDown));
+    SubscribeToEvent(E_MENU, URHO3D_HANDLER(Battler, HandleMenuEvent));
+    SubscribeToEvent(E_POSTRENDERUPDATE, URHO3D_HANDLER(Battler, HandlePostRenderUpdate));
+}
+
+
+void Battler::SetWindowTitleAndIcon()
+{
+    Image *icon = TheCache->GetResource<Image>("Textures/TacticsVictoryIcon.png");
+    TheGraphics->SetWindowIcon(icon);
+    SetWindowText(FindWindow(NULL, "Battler"), TEXT("Тактика Победы"));
+}
+
+
+void Battler::CreateConsoleAndDebugHud()
+{
+    XMLFile *xmlFile = TheCache->GetResource<XMLFile>("UI/ConsoleStyle.xml");
+
+    TheEngineConsole = engine_->CreateConsole();
+    TheEngineConsole->SetDefaultStyle(xmlFile);
+    TheEngineConsole->GetBackground()->SetOpacity(0.8f);
+
+    TheDebugHud = engine_->CreateDebugHud();
+    TheDebugHud->SetDefaultStyle(xmlFile);
+}
+
+
+void Battler::CreateEditorSession()
+{
+    if(!editor)
+    {
+        editor = new Editor(&TheEditor);
+    }
+    TheGuiEditor->SetVisible(true);
+    TheCamera->SetEnabled(true);
+    TheEditor->Run();
+}
+
+
+void Battler::OpenLog()
+{
+    log = new LogT(&TheLog);
+    TheLog->Open(GetTypeName() + ".log");
+    TheLog->SetLevel(LOG_DEBUG);
+}
---
+++ src/local/Client/Battler.h
@@ -0,0 +1,46 @@
+﻿// (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+class SceneT;
+
+
+class Battler : public Application
+{
+    URHO3D_OBJECT(Battler, Application);
+public:
+    Battler(Context* context);
+
+    virtual void Setup();
+    virtual void Start();
+    virtual void Stop();
+
+    bool drawDebug = false;
+
+private:
+
+    void OpenLog();
+    void RegistrationObjects();
+    void SetWindowTitleAndIcon();
+    void CreateConsoleAndDebugHud();
+    void CreateEditorSession();
+    void SubscribeToEvents();
+    void SetLocalization();
+    void CreateScriptSystem();
+    void GetSubsystems();
+    void TuneEngineParameters();
+    void CreateGUI();
+
+    void HandleMenuEvent(StringHash, VariantMap&);
+    void HandlePostRenderUpdate(StringHash, VariantMap&);
+    void HandleKeyDown(StringHash, VariantMap&);
+
+    SharedPtr<LogT>     log;
+    SharedPtr<SceneC>   scene;
+    SharedPtr<ServerC>  server;
+    SharedPtr<Mouse>    mouse;
+    SharedPtr<Settings> settings;
+    SharedPtr<MenuT>    menu;
+    SharedPtr<GUI>      gui;
+    SharedPtr<Editor>   editor;
+};
