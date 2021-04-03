6241bb0774310689c2c38f14c0b5ddc29698874e
Sasha7b9 <Sasha7b9@tut.by>
2021-04-03 12:57:41 +0300

    *

--- src/common/Controller/stdafx.h
+++ src/common/Controller/stdafx.h
@@ -1,2 +1,19 @@
 // 2021/04/02 17:15:01 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
+
+#pragma warning(push, 0)
+
+#define _WINSOCKAPI_
+#include <windows.h>
+
+#include <cstdarg>
+#include <iostream>
+#include <locale>
+#include <mutex>
+#include <vector>
+
+#pragma warning(pop)
+
+#include "defines.h"
+
+#include "Utils/Log_.h"
