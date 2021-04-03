bc4fd5ffd9b6d89930bac3566d0c00ae79da4af1
Sasha7b9 <Sasha7b9@tut.by>
2021-04-03 15:54:11 +0300

    *

---
+++ src/common/Monitor/Monitor.cpp
@@ -0,0 +1,18 @@
+// 2021/04/02 17:14:22 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "stdafx.h"
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    LogRAW::Create("Controller.log");
+
+    LOGWRITE("Start Controller");
+
+    TheConfig.Load("Contoller.cfg");
+
+    TheMaster.Connnect();
+
+    return 0;
+}
