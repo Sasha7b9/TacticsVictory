44d4127f2e955dc723aae40a6ef9bc5481c7261e
Sasha7b9 <Sasha7b9@tut.by>
2021-04-04 12:45:56 +0300

    Всё компилируется

--- src/common_files/GUI/Menu/WindowMenu_.cpp
+++ src/common_files/GUI/Menu/WindowMenu_.cpp
@@ -1,7 +1,7 @@
 ﻿// (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "stdafx.h"
 #include "GUI/Controls/Buttons/ButtonSwitch_.h"
-#include "GUI/Menu/MenuStart.h"
+#include "GUI/Menu/WindowMenu_.h"
 
 
 WindowMenu::WindowMenu() : WindowT(TheContext)
