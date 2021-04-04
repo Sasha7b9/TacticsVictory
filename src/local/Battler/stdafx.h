9e65cf0280556d8e6aa93be476f85fa8989aaae5
Sasha7b9 <Sasha7b9@tut.by>
2021-04-04 09:22:00 +0300

    *

--- src/local/Battler/stdafx.h
+++ src/local/Battler/stdafx.h
@@ -3,58 +3,35 @@
 
 #pragma warning(push, 0)
 
-#include <Urho3D/AngelScript/Addons.h>
 #include <Urho3D/AngelScript/APITemplates.h>
-#include <Urho3D/AngelScript/Script.h>
-#include <Urho3D/AngelScript/ScriptFile.h>
-#include <Urho3D/AngelScript/ScriptInstance.h>
 #include <Urho3D/Audio/Audio.h>
 #include <Urho3D/Audio/Sound.h>
 #include <Urho3D/Audio/SoundListener.h>
-#include <Urho3D/Audio/SoundSource.h>
 #include <Urho3D/Audio/SoundSource3D.h>
-#include <Urho3D/Core/Mutex.h>
 #include <Urho3D/Core/Profiler.h>
-#include <Urho3D/Core/Thread.h>
 #include <Urho3D/Engine/Application.h>
 #include <Urho3D/Engine/DebugHud.h>
-#include <Urho3D/Engine/Engine.h>
 #include <Urho3D/Engine/EngineDefs.h>
 #include <Urho3D/Engine/Console.h>
-#include <Urho3D/Graphics/BillboardSet.h>
 #include <Urho3D/Graphics/Camera.h>
 #include <Urho3D/Graphics/CustomGeometry.h>
 #include <Urho3D/Graphics/DebugRenderer.h>
-#include <Urho3D/Graphics/DecalSet.h>
-#include <Urho3D/Graphics/Drawable.h>
 #include <Urho3D/Graphics/Geometry.h>
 #include <Urho3D/Graphics/Graphics.h>
-#include <Urho3D/Graphics/GraphicsDefs.h>
 #include <Urho3D/Graphics/IndexBuffer.h>
 #include <Urho3D/Graphics/Model.h>
 #include <Urho3D/Graphics/Octree.h>
-#include <Urho3D/Graphics/OctreeQuery.h>
 #include <Urho3D/Graphics/ParticleEffect.h>
 #include <Urho3D/Graphics/ParticleEmitter.h>
-#include <Urho3D/Graphics/RenderSurface.h>
 #include <Urho3D/Graphics/Texture2D.h>
-#include <Urho3D/Graphics/Texture.h>
 #include <Urho3D/Graphics/Renderer.h>
-#include <Urho3D/Graphics/StaticModel.h>
-#include <Urho3D/Graphics/VertexBuffer.h>
 #include <Urho3D/Graphics/Zone.h>
 #include <Urho3D/Input/Input.h>
-#include <Urho3D/Input/InputEvents.h>
-#include <Urho3D/Math/MathDefs.h>
-#include <Urho3D/Math/Random.h>
 #include <Urho3D/UI/UI.h>
-#include <Urho3D/UI/Button.h>
-#include <Urho3D/UI/Cursor.h>
 #include <Urho3D/UI/DropDownList.h>
 #include <Urho3D/UI/FileSelector.h>
 #include <Urho3D/UI/LineEdit.h>
 #include <Urho3D/UI/ScrollBar.h>
-#include <Urho3D/UI/Sprite.h>
 #include <Urho3D/UI/Slider.h>
 #include <Urho3D/UI/Font.h>
 #include <Urho3D/UI/Text.h>
@@ -64,35 +41,22 @@
 #include <Urho3D/Urho2D/StaticSprite2D.h>
 #include <Urho3D/Urho2D/Sprite2D.h>
 #include <Urho3D/IO/IOEvents.h>
-#include <Urho3D/IO/File.h>
 #include <Urho3D/IO/FileSystem.h>
-#include <Urho3D/IO/Log.h>
 #include <Urho3D/IO/MemoryBuffer.h>
-#include <Urho3D/Math/Sphere.h>
-
 #ifdef SendMessage
 #undef SendMessage
 #endif
-
 #include <Urho3D/Network/Network.h>
 #include <Urho3D/Network/NetworkEvents.h>
 #include <Urho3D/Network/Protocol.h>
-#include <Urho3D/Physics/CollisionShape.h>
 #include <Urho3D/Physics/PhysicsWorld.h>
-#include <Urho3D/Physics/RigidBody.h>
-#include <Urho3D/Physics/PhysicsEvents.h>
 #include <Urho3D/Resource/ResourceEvents.h>
 #include <Urho3D/Resource/ResourceCache.h>
-#include <Urho3D/Resource/XMLFile.h>
 #include <Urho3D/Resource/Localization.h>
-#include <Urho3D/Resource/JSONFile.h>
 #include <Urho3D/Scene/LogicComponent.h>
 #include <Urho3D/Scene/Scene.h>
 #include <Urho3D/Scene/SceneEvents.h>
-#include <Urho3D/Core/Timer.h>
 #include <Urho3D/Core/CoreEvents.h>
-#include <Urho3D/Core/ProcessUtils.h>
-#include <Urho3D/DebugNew.h>
 
 using namespace Urho3D;
 
