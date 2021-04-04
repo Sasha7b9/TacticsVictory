1ee55b7b1085f4073f9cf6eec98db051a2291388
Sasha7b9 <Sasha7b9@tut.by>
2021-04-04 16:43:07 +0300

    *

--- src/common/Uploader/stdafx.h
+++ src/common/Uploader/stdafx.h
@@ -1,13 +1,19 @@
 // 2021/04/02 17:15:01 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
 
+#ifdef WIN32
 #pragma warning(push, 0)
+#endif
 
 #include <rapidjson/rapidjson.h>
 #include <rapidjson/document.h>
 
 #define _WINSOCKAPI_
 #include <windows.h>
+
+#include <Urho3D/Math/Vector2.h>
+
+using namespace Urho3D;
 
 #include <cstdarg>
 #include <locale>
@@ -18,7 +24,9 @@
 #include <sockpp/tcp_acceptor.h>
 #include <sockpp/tcp_connector.h>
 
+#ifdef WIN32
 #pragma warning(pop)
+#endif
 
 #include "defines.h"
 
