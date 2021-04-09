2884cb312517ac41cafd39c7f34fbde6c3e20885
Sasha7b9 <Sasha7b9@tut.by>
2021-04-09 16:06:41 +0300

    *

--- src/common_files/Network/Other/Server_.h
+++ src/common_files/Network/Other/Server_.h
@@ -22,9 +22,11 @@ public:
 
     static void HandlerReceivedSocket(AcceptorTCP::Socket &socket, pchar symbols, int number);
 
-    bool run = true;
-
     static std::map<std::string, pFuncVV> map;    // Здесь хранятся обработчики запросов по первому слову
 
     static std::map<std::string, ServerInfo> livingrooms;   // Здесь хранится информация о подключенных клиентах
+
+private:
+
+    bool run = true;
 };
