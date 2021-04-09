cc6e5c2fe15ffaaa3528cba3287961b5ddceea86
Sasha7b9 <Sasha7b9@tut.by>
2021-04-09 11:31:28 +0300

    *

--- src/common_files/Network/Other/Server_.cpp
+++ src/common_files/Network/Other/Server_.cpp
@@ -73,6 +73,12 @@ static void ThreadConnect(ConnectorTCP *conn_out, ConnectorTCP *conn_in,
     {
         std::string id = conn_out->Receive();           // После подключения получем id, который передадим серверу для
                                                         // идентификации conn_in
+
+        if (id.empty())
+        {
+            LOGERROR("Port ID not received for incoming messages");
+        }
+
         if (conn_in->Connect(host, (uint16)(port + 1))) // Следующий по порядку номер порта будет использоваться
         {                                               // для приёма ответных сообщений
             conn_in->Transmit(id);                      // После подключения передаём полученный id, чтобы сервер мог
