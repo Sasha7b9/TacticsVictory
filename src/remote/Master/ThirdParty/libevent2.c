a08e190d173c4345baee7293a0bff29b33c91f0a
Sasha7b9 <Sasha7b9@tut.by>
2021-04-10 19:14:34 +0300

    *

--- src/remote/Master/ThirdParty/libevent2.c
+++ src/remote/Master/ThirdParty/libevent2.c
@@ -1,6 +1,7 @@
 // 2021/04/10 18:42:52 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
 #pragma warning(push, 0)
+#pragma warning(disable : 4706)
 
 #include "../../ThirdParty/libevent/wepoll.c"
 #include "../../ThirdParty/libevent/bufferevent_pair.c"
--- src/remote/Master/ThirdParty/libevent3.c
+++ src/remote/Master/ThirdParty/libevent3.c
@@ -1,6 +1,7 @@
 // 2021/04/10 18:42:52 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
 #pragma warning(push, 0)
+#pragma warning(disable : 4702 4706)
 
 #include "../../ThirdParty/libevent/bufferevent_filter.c"
 #include "../../ThirdParty/libevent/event.c"
