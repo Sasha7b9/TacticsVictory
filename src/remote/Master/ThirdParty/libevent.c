304a48148da2e915f3caf6aa45b03dccead800ce
Sasha7b9 <Sasha7b9@tut.by>
2021-04-10 18:53:31 +0300

    *

---
+++ src/remote/Master/ThirdParty/libevent.c
@@ -0,0 +1,24 @@
+// 2021/04/10 18:42:52 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+
+#pragma warning(push, 0)
+
+#include "../../ThirdParty/libevent/bufferevent_async.c"
+#include "../../ThirdParty/libevent/buffer.c"
+#include "../../ThirdParty/libevent/buffer_iocp.c"
+#include "../../ThirdParty/libevent/bufferevent.c"
+#include "../../ThirdParty/libevent/bufferevent_ratelim.c"
+#include "../../ThirdParty/libevent/bufferevent_sock.c"
+#include "../../ThirdParty/libevent/epoll.c"
+#include "../../ThirdParty/libevent/event_iocp.c"
+#include "../../ThirdParty/libevent/evmap.c"
+#include "../../ThirdParty/libevent/evthread.c"
+#include "../../ThirdParty/libevent/evutil.c"
+#include "../../ThirdParty/libevent/evutil_rand.c"
+#include "../../ThirdParty/libevent/evutil_time.c"
+#include "../../ThirdParty/libevent/listener.c"
+#include "../../ThirdParty/libevent/log.c"
+#include "../../ThirdParty/libevent/signal.c"
+#include "../../ThirdParty/libevent/strlcpy.c"
+#include "../../ThirdParty/libevent/win32select.c"
+
+#pragma warning(pop)
---
+++ src/remote/Master/ThirdParty/libevent3.c
@@ -0,0 +1,8 @@
+// 2021/04/10 18:42:52 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+
+#pragma warning(push, 0)
+
+#include "../../ThirdParty/libevent/bufferevent_filter.c"
+#include "../../ThirdParty/libevent/event.c"
+
+#pragma warning(pop)
