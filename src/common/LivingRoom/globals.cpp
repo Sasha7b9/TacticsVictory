1c8a7a3d43969b9292f048986802cf7aae57d88c
Sasha7b9 <Sasha7b9@tut.by>
2021-04-11 19:24:44 +0300

    *

--- src/common/LivingRoom/globals.cpp
+++ src/common/LivingRoom/globals.cpp
@@ -1,6 +1,8 @@
 // 2021/04/03 12:59:49 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "stdafx.h"
+#include "FileSystem/ConfigurationFile_.h"
+#include "Network/Other/ServerConnector_.h"
 
 
 ConfigurationFile TheSettings;
-ServerConnector           TheMasterServer;
+ServerConnector   TheMasterServer;
