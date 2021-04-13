057221c7653fdb5657d8649ade583e0d10d93ef3
Sasha7b9 <Sasha7b9@tut.by>
2021-04-13 14:20:27 +0300

    *

--- src/common_files/GUI/Controls/Window_.h
+++ src/common_files/GUI/Controls/Window_.h
@@ -2,11 +2,11 @@
 #pragma once
 #include "GUI/Controls/DropDownListWithTextAndButton_.h"
 #include "GUI/Controls/Label_.h"
+#include "GUI/Controls/Buttons/ButtonToggled_.h"
 #include "GUI/Logic/LineTranslator2D_.h"
 
 
 class ButtonT;
-class ButtonToggled;
 class SliderWithTextAndButtons;
 
 
