8e34de3ff6eab6924c20c1145e1479c7b4385536
Sasha7b9 <Sasha7b9@tut.by>
2021-04-14 17:05:02 +0300

    *

--- src/common/Monitor/globals.cpp
+++ src/common/Monitor/globals.cpp
@@ -1,5 +1,6 @@
 // 2021/04/03 12:59:49 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "stdafx.h"
+#include "FileSystem/ConfigurationFile_.h"
 #include "Network/Other/ServerConnector_.h"
 
 
