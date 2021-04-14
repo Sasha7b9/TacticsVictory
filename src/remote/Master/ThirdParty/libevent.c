4fcf96bb2a32840d9e3cf67582c4f9b9cbfa69af
Sasha7b9 <Sasha7b9@tut.by>
2021-04-14 19:44:57 +0000

    *

--- src/remote/Master/ThirdParty/libevent.c
+++ src/remote/Master/ThirdParty/libevent.c
@@ -1,7 +1,9 @@
 // 2021/04/10 18:42:52 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
+#ifdef WIN32
 #pragma warning(push, 0)
 #pragma warning(disable : 4702 4706)
+#endif
 
 #include "../../ThirdParty/libevent/bufferevent_async.c"
 #include "../../ThirdParty/libevent/buffer.c"
@@ -22,4 +24,6 @@
 #include "../../ThirdParty/libevent/strlcpy.c"
 #include "../../ThirdParty/libevent/win32select.c"
 
+#ifdef WIN32
 #pragma warning(pop)
+#endif
