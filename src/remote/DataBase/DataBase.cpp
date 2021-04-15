8e5f0ff4162aa5fe3d17ac110f3757fc2c3b9b1c
Sasha7b9 <Sasha7b9@tut.by>
2021-04-15 22:24:24 +0300

    *

--- src/remote/DataBase/DataBase.cpp
+++ src/remote/DataBase/DataBase.cpp
@@ -1,7 +1,21 @@
-
 // 2021/04/02 17:14:22 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "stdafx.h"
 #include "FileSystem/ConfigurationFile_.h"
+#include "SQLite/src/sqlite3.h"
+
+
+static int callback(void * /*NotUsed*/, int argc, char **argv, char **azColName) {
+    int i;
+
+    for (i = 0; i < argc; i++)
+    {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+
+    printf("\n");
+
+    return 0;
+}
 
 
 int main()
@@ -12,9 +26,29 @@ int main()
 
     LOGWRITE("Start DataBase");
 
-    TheConfig.Load("DataBase.cfg");
+    sqlite3 *db = nullptr;
 
-//    TheMasterServer.Connect(TheConfig.GetString("address master"));
+    if (sqlite3_open("db.sqlite3", &db) != 0)
+    {
+        LOGERRORF("Can't open database: %s", sqlite3_errmsg(db));
+    }
+
+    char *sql = "CREATE TABLE COMPANY("    \
+        "ID INT PRIMARY KEY     NOT NULL," \
+        "NAME           TEXT    NOT NULL," \
+        "AGE            INT     NOT NULL," \
+        "ADDRESS        CHAR(50),"         \
+        "SALARY         REAL );";
+
+    char *zErrMsg = 0;
+
+    if (sqlite3_exec(db, sql, callback, 0, &zErrMsg) != SQLITE_OK)
+    {
+        LOGERRORF("SQL error: %s\n", zErrMsg);
+    }
+
+    sqlite3_close(db);
 
     return 0;
 }
+    
