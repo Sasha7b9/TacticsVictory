d4640a330ff8aa785964e656171074d93f92d900
Sasha7b9 <Sasha7b9@tut.by>
2021-04-18 12:13:37 +0300

    *

--- src/common/LivingRoom/stdafx.h
+++ src/common/LivingRoom/stdafx.h
@@ -5,7 +5,8 @@
 #pragma warning(push, 0)
 #else
 #pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wclass-memaccess"
+//#pragma GCC diagnostic ignored "-Wclass-memaccess"
+#pragma GCC diagnostic ignored "-Wall"
 #endif
 
 #include <rapidjson/rapidjson.h>
