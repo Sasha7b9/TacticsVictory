e9b0e898bc02b92b8d9b6efff87c8c0ccdb5469c
Sasha7b9 <Sasha7b9@tut.by>
2021-04-21 21:27:01 +0300

    *

--- src/common_files/Network/Other/SocketsTCP_.h
+++ src/common_files/Network/Other/SocketsTCP_.h
@@ -5,38 +5,3 @@
 
 
 
-
-
-// Сервер
-class AcceptorTCP
-{
-public:
-
-    struct Socket
-    {
-        std::unique_ptr<sockpp::inet_address>  peer;            // Здесь хранится адрес подключённого клиента
-        sockpp::tcp_socket sock;
-        void Transmit(pchar string);
-        void Transmit(const std::string &data);
-    };
-
-public:
-
-    bool Bind(uint16 port);
-
-    bool Accept(Socket &socket);
-
-private:
-
-    std::unique_ptr<sockpp::tcp_acceptor> connection;
-
-    struct CompareSocket
-    {
-        bool operator()(const sockpp::tcp_socket *lhs, const sockpp::tcp_socket *rhs) const
-        {
-            return lhs->peer_address().address() < rhs->peer_address().address();
-        }
-    };
-
-    std::map<sockpp::tcp_socket *, sockpp::inet_address, CompareSocket> sockets;
-};
