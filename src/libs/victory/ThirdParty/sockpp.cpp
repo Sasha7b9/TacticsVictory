5e66afc899a9bf5ac7256bdd7edf12555ae255bd
Sasha7b9 <Sasha7b9@tut.by>
2021-04-22 15:21:34 +0300

    *

--- src/libs/victory/ThirdParty/sockpp.cpp
+++ src/libs/victory/ThirdParty/sockpp.cpp
@@ -1,7 +1,11 @@
 // 2021/04/01 17:30:33 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "stdafx.h"
 
+#ifdef WIN32
 #pragma warning(push, 0)
+#else
+#endif
+
 
 #include "../../ThirdParty/sockpp/src/socket.cpp"
 #include "../../ThirdParty/sockpp/src/acceptor.cpp"
@@ -10,4 +14,8 @@
 #include "../../ThirdParty/sockpp/src/exception.cpp"
 #include "../../ThirdParty/sockpp/src/inet_address.cpp"
 
+
+#ifdef WIN32
 #pragma warning(pop)
+#else
+#endif
