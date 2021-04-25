3c4ac64b70f6017e6ae4393b00ba6c8fec8726fa
Sasha7b9 <Sasha7b9@tut.by>
2021-04-25 17:18:30 +0000

    -

--- scripts/Linux/send.sh
+++ scripts/Linux/send.sh
@@ -1,4 +1,1 @@
-#data=$(cat build.log)
-#curl -X POST \
-#,    -F document=@"build.log" '{"chat_id": "546264605"
 curl -F document=@"build.log" https://api.telegram.org/bot1796168405:AAGOO4O_gydbNEY-J96KspPdmlsoPFlrVNY/sendDocument?chat_id=546264605
