968c8d27c8e2260595d7193e88539fecdd5b36a4
Sasha7b9 <Sasha7b9@tut.by>
2021-04-25 16:12:27 +0000

    *

--- scripts/Linux/auto.sh
+++ scripts/Linux/auto.sh
@@ -7,7 +7,7 @@ do
     FILESIZE=$(stat -c%s "$FILENAME")
     if [ $FILESIZE != "20" ]
     then
-        time ./assembly.sh build all
+        time ./assembly.sh build all > build.txt
     fi
     sleep 1
 done
