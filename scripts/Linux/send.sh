9eeac4626db1c58751e4a91450eb1d46946248e4
Sasha7b9 <Sasha7b9@tut.by>
2021-04-25 16:23:26 +0000

    *

--- scripts/Linux/send.sh
+++ scripts/Linux/send.sh
@@ -1,5 +1,5 @@
 data=$(cat build.log)
 curl -X POST \
     -H 'Content-Type: application/json' \
-    -d '{"chat_id": "546264605", "text": "'$data'" , "disable_notification": false}' \
+    -d '{"chat_id": "546264605", "text": "succesefull build" , "disable_notification": false}' \
     https://api.telegram.org/bot1796168405:AAGOO4O_gydbNEY-J96KspPdmlsoPFlrVNY/sendMessage
