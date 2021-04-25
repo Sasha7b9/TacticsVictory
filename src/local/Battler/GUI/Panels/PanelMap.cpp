e15200ba1f25979d0c2a1e35217930ea40a07e36
Sasha7b9 <Sasha7b9@tut.by>
2021-04-25 16:43:19 +0300

    -

--- src/local/Battler/GUI/Panels/PanelMap.cpp
+++ src/local/Battler/GUI/Panels/PanelMap.cpp
@@ -178,7 +178,10 @@ void PanelMap::Update(float dT)
 
     SharedPtr<Texture2D> texture(new Texture2D(TheContext));
 
+#ifdef WIN32
     texture->SetSize(GetWidth(), GetHeight(), D3DFMT_X8R8G8B8);
+#endif
+
     texture->SetData((SharedPtr<Image>)image);
 
     SetTexture(texture);
