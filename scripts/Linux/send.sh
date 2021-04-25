96dcc3b66e2c1db8390e4576076ed487ad351e0b
Sasha7b9 <Sasha7b9@tut.by>
2021-04-25 16:04:18 +0000

    ЗАготовка скприпта для телеграм-бота

---
+++ scripts/Linux/send.sh
@@ -0,0 +1,4 @@
+curl -X POST \
+    -H 'Content-Type: application/json' \
+    -d '{"chat_id": "546264605", "text": {$1} , "disable_notification": false}' \
+    https://api.telegram.org/bot1796168405:AAGOO4O_gydbNEY-J96KspPdmlsoPFlrVNY/sendMessage
