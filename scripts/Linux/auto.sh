7d7c487f4cae60e2d4ab0aa231d61ef45967e866
Sasha7b9 <Sasha7b9@tut.by>
2021-04-25 17:06:31 +0000

    *

--- scripts/Linux/auto.sh
+++ scripts/Linux/auto.sh
@@ -7,8 +7,14 @@ do
     FILESIZE=$(stat -c%s "$FILENAME")
     if [ $FILESIZE != "20" ]
     then
-        time ./assembly.sh build all > build.log
-        ./send.sh
+        rm build.log
+        time ./assembly.sh build all 2>> build.log
+        FILENAME=build.log
+        FILESIZE=$(stat -c$s "$FILENAME")
+        if [ $FILESIZE != "0" ]
+        then
+            ./send.sh
+        fi
     fi
     sleep 1
 done
