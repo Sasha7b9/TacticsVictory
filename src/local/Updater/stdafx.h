dcc97a50c732518cebcff7b0af13cbf2aa91d40a
Sasha7b9 <Sasha7b9@tut.by>
2021-04-25 15:08:39 +0300

    *

--- src/local/Updater/stdafx.h
+++ src/local/Updater/stdafx.h
@@ -1,7 +1,10 @@
 // 2021/04/02 17:15:01 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
 
+#ifdef WIN32
 #pragma warning(push, 0)
+#else
+#endif
 
 #include <rapidjson/rapidjson.h>
 #include <rapidjson/document.h>
@@ -18,7 +21,10 @@ using namespace Urho3D;
 #include <sockpp/tcp_acceptor.h>
 #include <sockpp/tcp_connector.h>
 
+#ifdef WIN32
 #pragma warning(pop)
+#else
+#endif
 
 #include "defines.h"
 #include "globals.h"
