397e305a60aafea05df0da50ef9113e1d87f6c99
Sasha7b9 <Sasha7b9@tut.by>
2021-04-26 21:31:41 +0000

    -

--- scripts/Linux/auto.sh
+++ scripts/Linux/auto.sh
@@ -7,19 +7,22 @@ do
     FILESIZE=$(stat -c%s "$FILENAME")
     if [ $FILESIZE != "20" ]
     then
-        rm fail.build
+
         time ./assembly.sh build all 2>> fail.build
         FILENAME=fail.build
         FILESIZE=$(stat -c%s "$FILENAME")
+
         if [ $FILESIZE != "0" ]
         then
             ./send_fail.sh
+            rm fail.build
         fi
-        rm good.build
+
         if [ $FILESIZE == "0" ]
         then
             time ./assembly.sh build all > good.build
             ./send_good.sh
+            rm good.build
         fi
     fi
     sleep 1
