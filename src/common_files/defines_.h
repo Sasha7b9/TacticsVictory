82d04342abd3f547006324d454ee813c67450746
Sasha7b9 <Sasha7b9@tut.by>
2021-04-28 08:13:56 +0300

    -

--- src/common_files/defines_.h
+++ src/common_files/defines_.h
@@ -1,5 +1,5 @@
 // 2021/02/23 22:42:59 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
-#pr agma once
+#pragma once
 
 // В этой переменой ноды будет храниться имя типа игрового объекта
 #define VAR_OBJECT_TYPE_NAME    "Object type name"
