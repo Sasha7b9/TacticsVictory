fe756d31584dfd4e9c3204e17d346cb7b3d6b8b6
Sasha7b9 <Sasha7b9@tut.by>
2021-04-29 22:16:03 +0300

    *

--- src/libs/victory/Network/Other/NetworkTypes_v.cpp
+++ src/libs/victory/Network/Other/NetworkTypes_v.cpp
@@ -160,7 +160,7 @@ int SockAddrIn::RecvFrom(evutil_socket_t socket, char *buffer, int size_buffer)
 {
     socklen_t size = sizeof(struct sockaddr);
 
-    int received = recvfrom((SOCKET)socket, buffer, size_buffer, 0, GetAddr(), &size);
+    int received = recvfrom((socket_t)socket, buffer, size_buffer, 0, GetAddr(), &size);
 
     if (received < 0)
     {
