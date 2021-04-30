4fcecb5b6d3d4e2a578d0485ba797efb5af79456
Sasha7b9 <Sasha7b9@tut.by>
2021-04-30 09:42:29 +0300

    *

--- src/libs/victory/Network/Other/ConnectorTCP_v.cpp
+++ src/libs/victory/Network/Other/ConnectorTCP_v.cpp
@@ -246,8 +246,6 @@ void ConnectorTCP::Disconnect()
 
 uint ConnectorTCP::SendRequest(pchar request, const void *buffer, uint size_buffer)
 {
-    LOGWRITEF("request \"%s\"", request);
-
     mutex.lock();
 
     last_request_id++;
