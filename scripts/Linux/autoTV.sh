04448899ba1e5a51c38d8f4d69d191750706e6a9
Sasha7b9 <Sasha7b9@tut.by>
2021-05-01 16:56:12 +0300

    *

--- scripts/Linux/autoTV.sh
+++ scripts/Linux/autoTV.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
+
+function Build
+{
+    ./stop.sh all
+
+    echo
+    echo Start build ...
+
+    time ./assembly.sh build release 1>good.build  2>fail.build
+    FILENAME=fail.build
+    FILESIZE=$(stat -c%s "$FILENAME")
+
+    if [ $FILESIZE != "0" ]
+    then
+        ./send_fail.sh
+        rm fail.build
+    fi
+
+    if [ $FILESIZE == "0" ]
+    then
+        ./send_good.sh
+        rm good.build
+    fi
+
+    ./start.sh all    
+}
+
+Build
+
 while true
 do
     git pull > out.txt
@@ -8,28 +37,6 @@ do
     if [ $FILESIZE != "20" ]            #  20 - size string "Already up to date."
     then
 
-        ./stop.sh all
-
-        echo
-        echo Start build ...
-
-        time ./assembly.sh build release 1>good.build  2>fail.build
-        FILENAME=fail.build
-        FILESIZE=$(stat -c%s "$FILENAME")
-
-        if [ $FILESIZE != "0" ]
-        then
-            ./send_fail.sh
-            rm fail.build
-        fi
-
-        if [ $FILESIZE == "0" ]
-        then
-            ./send_good.sh
-            rm good.build
-        fi
-
-        ./start.sh all
 
     fi
     sleep 1
